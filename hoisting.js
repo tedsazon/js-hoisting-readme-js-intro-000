36ab4d925a9debaaf6ad8f021075133102c1f6d8
Ted Sazon <tedsazon@gmail.com>
2018-11-20 21:54:30 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -17,10 +17,9 @@ function crazy(){
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+  sayMy();
   function sayMy() {
     var name = "Kristin";
     console.log(name);
-  }   sayMy();
+  }  
 }
