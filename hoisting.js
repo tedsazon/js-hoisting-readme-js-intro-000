ef9eaaecf2e756bdbaadc6cadd31e4a987339240
Ted Sazon <tedsazon@gmail.com>
2018-11-20 21:52:54 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,26 @@
-function callMe() {
+
+function callMe(){
   var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-
 }
 
-
-
-function crazy() {
+function crazy(){
+  //fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  } thisIsCrazy();
+  }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
     var name = "Kristin";
-
     console.log(name);
-  }
+  }   sayMy();
 }
